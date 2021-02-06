c887e71de9f3e736b51d7b762228339fef3fd529
Monsterkill111 <77743677+Monsterkill111@users.noreply.github.com>
2021-02-06 06:16:53 +0300

    Add files via upload

---
+++ Task2_2/FileHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Task2_2
+{
+    public class FileHandler
+    {
+        public void ProcessFile(string fileName, char ch, bool begin)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(fileName);
+
+                StreamWriter sw = File.CreateText("../../../output.txt");
+
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    line = Regex.Replace(line, @"\s+", " ");
+                    foreach (var curr in line.Split(" "))
+                    {
+                        if (IsWordContainRequiredChar(curr, ch, begin))
+                        {
+                            sw.Write(curr + " ");
+                        }   
+                    }
+
+                    line = sr.ReadLine();
+                }
+
+
+                sr.Close();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public bool IsWordContainRequiredChar(string word, char ch, bool begin)
+        {
+            char[] chars = word.ToCharArray();
+
+            if (begin)
+            {
+                if (Char.IsLetter(chars[0]))
+                {
+                    return char.ToUpperInvariant(chars[0]) == char.ToUpperInvariant(ch);
+                }
+                else
+                {
+                    return char.ToUpperInvariant(chars[1]) == char.ToUpperInvariant(ch);
+                }
+            }
+            else
+            {
+                if (Char.IsLetter(chars[^1]))
+                {
+                    return char.ToUpperInvariant(chars[^1]) == char.ToUpperInvariant(ch);
+                }
+                else
+                {
+                    return char.ToUpperInvariant(chars[^2]) == char.ToUpperInvariant(ch);
+                }
+                
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Task2_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1) Default Russian 'С'");
+            Console.WriteLine("2) Choose any char");
+
+            int choose = Convert.ToInt32(Console.ReadLine());
+
+            char ch;
+            if (choose == 1)
+            {
+                ch = 'С';
+            }
+            else
+            {
+                Console.WriteLine("Write any char");
+                ch = Convert.ToChar(Console.ReadLine());
+            }
+
+
+            string path = "../../../";
+
+            Console.WriteLine("Write file name (input.txt)");
+            string fileName = Console.ReadLine();
+
+            string filePath = path + fileName;
+
+            Console.WriteLine("Choose char place:");
+            Console.WriteLine("1) Begin");
+            Console.WriteLine("2) End");
+
+            bool begin = Convert.ToInt32(Console.ReadLine()) == 1 ? true : false;
+
+            FileHandler fileHandler = new FileHandler();
+
+            fileHandler.ProcessFile(filePath, ch, begin);
+        }
+    }
+}
